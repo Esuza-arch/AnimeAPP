0b2dae0db492f35df095b9613672e7cb965eeb3a
Yakubu Esuza Odiara <yakubuesuza@gmail.com>
2024-10-21 20:39:55 +0300

    added const themeToggle

--- index.js
+++ index.js
@@ -3,5 +3,6 @@ document.addEventListener("DOMContentLoaded", () =>{
   const animeContainer = document.getElemenetById("anime-container");
   const searchInput = document.getElementById("searchInput");
   const ratingFilter = document.getElementById("ratingFilter");
+  const themeToggle = document.getElementById("theme-toggle");
   
 })
