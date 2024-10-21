86a957e8ed8ce9e2cb52d3a345cddceeba50817b
Yakubu Esuza Odiara <yakubuesuza@gmail.com>
2024-10-21 21:00:02 +0300

    added fetch()

--- index.js
+++ index.js
@@ -10,6 +10,8 @@ document.addEventListener("DOMContentLoaded", () =>{
 
   searchInput.addEventListener("keyup", () => {
     const query = serchInput.value;
-    
+    if (query) {
+      fetch()
+    }
   })
 })
