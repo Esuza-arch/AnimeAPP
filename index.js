1a99649101a39cc63924c45d217a6d695be1e875
Yakubu Esuza Odiara <yakubuesuza@gmail.com>
2024-10-21 20:38:37 +0300

    added const ratingFilter

--- index.js
+++ index.js
@@ -2,5 +2,6 @@ document.addEventListener("DOMContentLoaded", () =>{
   const apiURL = "https://kitsu.io/api/edge/anime?filter[text]="
   const animeContainer = document.getElemenetById("anime-container");
   const searchInput = document.getElementById("searchInput");
+  const ratingFilter = document.getElementById("ratingFilter");
   
 })
