8c9f5cb8c022c7fb5e3db83413d81421609ad41f
Yakubu Esuza Odiara <yakubuesuza@gmail.com>
2024-10-21 20:33:17 +0300

    added const animeContainer

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 document.addEventListener("DOMContentLoaded", () =>{
   const apiURL = "https://kitsu.io/api/edge/anime?filter[text]="
+  const animeContainer = document.getElemenetById("anime-container");
   
 })
